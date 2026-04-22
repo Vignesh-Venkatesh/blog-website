6f9142bd457df48d264f90b3bd33459c4037ff4c
Vignesh Venkatesh <vv2455@nyu.edu>
2026-04-22 10:44:24 -0400

    modified design style

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,12 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/blog",
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -12,4 +16,6 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = {
+  blog,
+};
